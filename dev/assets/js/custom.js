40ae0f5653faee3888ab961f667e0729f6b68300
Alona <al90nov@gmail.com>
2024-07-03 21:08:35 +0200

    feat: header done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,12 @@
+  const burger = document.querySelector('.btn-burger');
+  const mobileContainer = document.querySelector('.mobile-container');
+  const body = document.querySelector('body')
+
+  function toggleClass() {
+    mobileContainer.classList.toggle('show')
+    body.classList.toggle('overlow-hidden')
+    burger.classList.toggle('active')
+  }
+
+  burger.addEventListener('click', toggleClass)
   
