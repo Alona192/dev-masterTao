f9a5a02814c01d327d11bfa21446233ec58ebad8
Alona <al90nov@gmail.com>
2024-07-10 12:31:56 +0200

    feat:first-screen

---
+++ dev/assets/js/custom.js
@@ -0,0 +1,40 @@
+  const burger = document.querySelector('.hamburger');
+  const mobileContainer = document.querySelector('.mobile-container');
+  const body = document.querySelector('body')
+
+  function toggleClass() {
+    mobileContainer.classList.toggle('show')
+    burger.classList.toggle('is-active')
+    body.classList.toggle('overlow-hidden')
+    
+  }
+
+  burger.addEventListener('click', toggleClass)
+
+  const swiper = new Swiper('.banner-slider', {
+    loop: true,
+    spaceBetween: 20,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      1025: {
+        spaceBetween: 50,
+        navigation: {
+          enabled: true,
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      },
+    }
+   
+  });
+  
