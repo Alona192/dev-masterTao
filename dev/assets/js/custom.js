e6fefeba6c9d623198a03a3ab956da487b6a155d
Alona <al90nov@gmail.com>
2024-07-19 12:30:37 +0200

    feat: advantages section

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -11,7 +11,7 @@
 
   burger.addEventListener('click', toggleClass);
 
-  const swiper = new Swiper('.banner-slider', {
+  const swiperBanner = new Swiper('.banner-slider', {
     loop: true,
     spaceBetween: 20,
 
@@ -50,3 +50,35 @@
       fileName.innerHTML = item.files[0].name;
     })
   })
+
+const advantagesSlideLength = document.querySelectorAll('.advantages .swiper-slide').length;
+console.log(advantagesSlideLength);
+
+const swiperAdvantages = new Swiper('#advantages', {
+    speed: 400,
+    spaceBetween: 12,
+    loop: true,
+    simulateTouch: true,
+    slidesPerView: 1,
+
+    pagination: {
+      el: '.swiper-pagination',
+      enabled: true,
+      clickable: true,
+    },
+
+    breakpoints: {
+      1025: {
+        spaceBetween: 0,
+        loop: false,
+        slidesPerView: 4,
+        slidesPerGroup: advantagesSlideLength,
+        simulateTouch: false,
+
+        pagination: {
+          enabled: false,
+        }
+      }
+    }
+  });
+  
